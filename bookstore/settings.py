97b81e39898d23fc88db075d69363d992bac5327
Gabriel Silva <gabrielssilva3107@gmail.com>
2025-04-10 19:18:19 -0300

    Corrigido nome do middleware do WhiteNoise

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
-    "whitenoise.middlerware.WhiteNoiseMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "bookstore.urls"
